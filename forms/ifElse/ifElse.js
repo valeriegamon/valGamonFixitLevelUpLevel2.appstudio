c2f082209a7b55682d13364df545abfbc622d782
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-09 23:33:10 -0600

    Update ifElse.js

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,21 +1,18 @@
-let name = prompt('What is your first name??');
-let state = prompt('What state are you from? (ie. NE) ');
-let temp = prompt('What Fahrenheit temperature is it outside right now? (ie. 50) ');
+let userName = prompt('What is your first name??');
+let userState = prompt('What state are you from? (ie. NE) ');
+let userTemp = Number(prompt('What Fahrenheit temperature is it outside right now? (ie. 50) '));
 
-const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1); 
-const stateCapitalized = state.charAt(0).toUpperCase() + state.charAt(1).toUpperCase(); 
+const nameCapitalized = (userName.charAt(0).toUpperCase() + name.slice(1)); 
+const stateCapitalized = (userState.toUpperCase()); 
 
-let outfit = [`Gosh ${name}! You need a coat and gloves! Its gonna be chilly ouside!!`,  `You should just need a light jacket ${name}.`,  `${name}, you should dress for warm weather with sandals, sunglasses and sunscreen.`,  `${name}, Stay away from the outdoors! Its WICKED HOT out there.'];
+let outfit = [`Gosh ${nameCapitalized}! You need to wear a warm coat, hat, scarf and gloves.!`,  `${nameCapitalized}, wear a warm coat but you won't need a hat, scarf or gloves.`,  `${nameCapitalized}, you should wear your warmest coat, a warm hat, a scarf, and warm gloves.`,  `${nameCapitalized}, wear a warm coat, hat and gloves. Maybe a scarf too.`];  
 
-
-
-if (temp <= 32) {
+if (userTemp < 32 && stateCapitalized == 'NE') {
     alert(outfit[0]);
-} else if (temp >= 32 && temp <=60) {
+} else if (userTemp >= 32 && userTemp <=50 && stateCapitalized == 'NE') {
     alert(outfit[1]);
-} else if (temp >= 61 && temp <=100) {
+} else if (userTemp >= 32 && userTemp <=50 && stateCapitalized == 'FL') {
     alert(outfit[2]);
-} else if (temp >=101) {
+} else if (userTemp >50 && userTemp <=70 && stateCapitalized == 'FL') {
     alert(outfit[3]);
 }
-
