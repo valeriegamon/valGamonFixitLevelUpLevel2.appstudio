e808df133134aea352ee85f65769cdce5441d069
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-09 22:25:56 -0600

    es yes

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,9 @@
+
+let name = prompt('What is your first name??');
+let state = prompt('What state are you from? (ie. NE) ');
+let temp = prompt('What Fahrenheit temperature is it outside right now? (ie. 50) ');
+
+let outfit = ('Gosh! You need a coat and gloves! Its gonna be chilly ouside!!', 'You should just need a light jacket.', 'You should dress for warm weather with sandals, sunglasses and sunscreen.', 'Stay away from the outdoors. Its WICKED HOT out there.');
+
+
+
