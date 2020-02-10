833ac9d8899be4659376576a86d5ed0265ddd5a6
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-09 22:57:23 -0600

    v

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -2,7 +2,10 @@ let name = prompt('What is your first name??');
 let state = prompt('What state are you from? (ie. NE) ');
 let temp = prompt('What Fahrenheit temperature is it outside right now? (ie. 50) ');
 
-let outfit = [`Gosh ${name}! You need a coat and gloves! Its gonna be chilly ouside!!`,  `You should just need a light jacket ${name}.`,  `${name}, you should dress for warm weather with sandals, sunglasses and sunscreen.`,  `${name}? Stay away from the outdoors! Its WICKED HOT out there.'];
+const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1); 
+const stateCapitalized = state.charAt(0).toUpperCase() + state.charAt(1).toUpperCase(); 
+
+let outfit = [`Gosh ${name}! You need a coat and gloves! Its gonna be chilly ouside!!`,  `You should just need a light jacket ${name}.`,  `${name}, you should dress for warm weather with sandals, sunglasses and sunscreen.`,  `${name}, Stay away from the outdoors! Its WICKED HOT out there.'];
 
 if (temp <= 32) {
     alert(outfit[0]);
